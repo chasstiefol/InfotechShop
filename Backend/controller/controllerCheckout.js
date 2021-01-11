4bf19ada5cd5565f8888939508dbebfd24b6c60b
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2021-01-11 23:01:30 +0700

    Update controllerCheckout.js

--- Backend/controller/controllerCheckout.js
+++ Backend/controller/controllerCheckout.js
@@ -1,8 +1,10 @@
-const asyncHandler = require('express-async-handler')
-const Akun = require('../models/Akun')
-const Pesanan = require('../models/Pesanan')
-const Bukti = require('../models/BuktiBayar')
-const uuid = require('uuid')
+const Produk = require("../models/Produk");
+const Cart = require("../models/KeranjangBelanja");
+const asyncHandler = require("express-async-handler");
+const Akun = require("../models/Akun");
+const Pesanan = require("../models/Pesanan");
+const Bukti = require("../models/BuktiBayar");
+const uuid = require("uuid");
 
 const tambahPesanan = asyncHandler(async (req, res) => {
   const {
@@ -12,19 +14,19 @@ const tambahPesanan = asyncHandler(async (req, res) => {
     hargaBarang,
     ongkir,
     totalPembayaran,
-  } = req.body
+  } = req.body;
 
-  const kode = uuid.v4()
-  const kodetanpastrip = kode.replace(/-/g, '')
-  const kodePembayaran = `${kodetanpastrip}${barangPesanan.idProduk}`
-  const pembeli = await Akun.findById(req.user._id).select('-password')
+  const kode = uuid.v4();
+  const kodetanpastrip = kode.replace(/-/g, "");
+  const kodePembayaran = `${kodetanpastrip}${barangPesanan.idProduk}`;
+  const pembeli = await Akun.findById(req.user._id).select("-password");
 
   if (barangPesanan && barangPesanan.length === 0) {
-    res.status(400)
-    throw new Error('Barang tidak ditemukan')
-    return
+    res.status(400);
+    throw new Error("Barang tidak ditemukan");
+    return;
   } else {
-    const order = await Pesanan.create({
+    const order = new Pesanan({
       barangPesanan,
       pembeli: pembeli,
       namaPembeli: pembeli.nama,
@@ -36,68 +38,79 @@ const tambahPesanan = asyncHandler(async (req, res) => {
       metodePembayaran,
       totalPembayaran,
       kodePembayaran: kodePembayaran,
-    })
+    });
 
-    res.status(201).json(order)
+    const buatPesanan = order.save();
+    res.status(201).json(buatPesanan);
   }
-})
+});
 
 const detailPesanan = asyncHandler(async (req, res) => {
-  const order = await Pesanan.findById(req.params.id)
+  const order = await Pesanan.findById(req.params.id).populated(
+    "pembeli",
+    "namaPembeli",
+    "emailPembeli",
+    "avatar"
+  );
 
   if (order) {
-    res.status(200).json(order)
+    res.status(200).json(order);
   } else {
-    res.status(404)
-    throw new Error('Pesanan tidak ditemukan')
+    res.status(404);
+    throw new Error("Pesanan tidak ditemukan");
   }
-})
+});
 
 const updatePesananDibayar = asyncHandler(async (req, res) => {
-  const order = await Pesanan.findById(req.params.id)
+  const order = await Pesanan.findById(req.params.id);
 
   if (order) {
-    order.sudahBayar = true
-    order.tanggalBayar = Date.now()
+    order.sudahBayar = true;
+    order.tanggalBayar = Date.now();
 
-    const statusPembayaran = await order.save()
-    res.status(201).json(statusPembayaran)
+    const statusPembayaran = await order.save();
+    res.status(201).json(statusPembayaran);
   } else {
-    res.status(404)
-    throw new Error('Pesanan tidak ditemukan')
+    res.status(404);
+    throw new Error("Pesanan tidak ditemukan");
   }
-})
+});
 
 const updatePesananDikirim = asyncHandler(async (req, res) => {
-  const order = await Pesanan.findById(req.params.id)
+  const order = await Pesanan.findById(req.params.id);
 
   if (order) {
-    order.sudahDikirim = true
-    order.tanggalDikirim = Date.now()
+    order.sudahDikirim = true;
+    order.tanggalDikirim = Date.now();
 
-    const statusPembayaran = await order.save()
-    res.status(201).json(statusPembayaran)
+    const statusPembayaran = await order.save();
+    res.status(201).json(statusPembayaran);
   } else {
-    res.status(404)
-    throw new Error('Pesanan tidak ditemukan')
+    res.status(404);
+    throw new Error("Pesanan tidak ditemukan");
   }
-})
+});
 
 const pesananSaya = asyncHandler(async (req, res) => {
-  const pesanan = await Pesanan.find({ pembeli: req.params.id })
-  res.json(pesanan)
-})
+  const pesanan = await Pesanan.find({ pembeli: req.user._id });
+  res.json(pesanan);
+});
 
 const seluruhPesanan = asyncHandler(async (req, res) => {
-  const pesanan = await Pesanan.find({})
-  res.json(pesanan)
-})
+  const pesanan = await Pesanan.find({}).populate(
+    "pembeli",
+    "namaPembeli",
+    "emailPembeli",
+    "avatar"
+  );
+  res.json(pesanan);
+});
 
 const buktiBayar = asyncHandler(async (req, res) => {
   // const files = req.files
   //gambar
   try {
-    let urls = []
+    let urls = [];
     // let berkas = async (path) => await upload(path)
     // for (const file of files) {
     //   const { path } = file
@@ -109,7 +122,7 @@ const buktiBayar = asyncHandler(async (req, res) => {
 
     if (urls) {
       // let body = req.body;
-      let pengguna = await Akun.findById(req.user._id).select('-password')
+      let pengguna = await Akun.findById(req.user._id).select("-password");
       // let bodyw = _.extend(body, { pengguna: pengguna }, { gambar: urls });
 
       let bukti = new Bukti({
@@ -118,22 +131,39 @@ const buktiBayar = asyncHandler(async (req, res) => {
         email: pengguna.email,
         bukti: req.body.bukti,
         tanggalBayar: Date.now,
-      })
+      });
       await bukti
         .save()
         .then((saved) => {
-          return res.json(saved)
+          return res.json(saved);
         })
         .catch((error) => {
-          return res.json(error)
-        })
+          return res.json(error);
+        });
     }
   } catch (error) {
-    res.status(400)
-    console.log('error: ', error)
-    return next(error)
+    res.status(400);
+    console.log("error: ", error);
+    return next(error);
   }
-})
+});
+
+const cariBukti = asyncHandler(async (req, res) => {
+  const bukti = await Bukti.findById(req.params.id);
+
+  if (bukti) {
+    res.json(bukti);
+    res.status(201);
+  } else {
+    res.status(404);
+    throw new Error("Bukti tidak ditemukan");
+  }
+});
+
+const seluruhBukti = asyncHandler(async (req, res) => {
+  const bukti = await Bukti.find({});
+  res.json(bukti);
+});
 
 module.exports = {
   tambahPesanan,
@@ -143,4 +173,6 @@ module.exports = {
   pesananSaya,
   seluruhPesanan,
   buktiBayar,
-}
+  cariBukti,
+  seluruhBukti,
+};
