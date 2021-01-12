abd81548da12edaf0dc241caae954fb18cbaf976
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2021-01-12 14:47:55 +0700

    Update index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -9,14 +9,14 @@ const {
   updateProfil,
   cariProfil,
 } = require("../controller/controllerPengguna");
-const { pelindung } = require("../middleware/validasi");
+const { pelindung, admin } = require("../middleware/validasi");
 
 router.route("/login/manual").post(authManual);
 router.route("/register/manual").post(registerManual).get(pelindung);
-router.route("/pengguna").get(seluruhProfil);
-router.route("/profil").get(profil);
-router.route("/hapus").delete(hapusProfil);
-router.route("/update").post(updateProfil);
-router.route("/cari").get(cariProfil);
+router.route("/pengguna").get(admin, seluruhProfil);
+router.route("/profil").get(pelindung, profil);
+router.route("/hapus").delete(admin, hapusProfil);
+router.route("/update").post(pelindung, updateProfil);
+router.route("/cari").get(admin, cariProfil);
 
 module.exports = router;
